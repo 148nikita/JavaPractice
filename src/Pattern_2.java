e4ae9fc49e4182532be7ba84bd71d3f2e95557e6
148nikita <nikita148chaurasia@gmail.com>
2026-03-15 21:39:46 +0530

    Pattern_2

--- src/Pattern_2.java
+++ src/Pattern_2.java
@@ -4,9 +4,8 @@ public class Pattern_2 {
     public static void main (String[] args){
         Scanner scn=new Scanner(System.in);
         int num=scn.nextInt();
-        for(int i=num;i>0;i--)
-        {
-            for(int j=1;j<=i;j++){
+        for(int i=num;i>0;i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*\t");
 
             }
